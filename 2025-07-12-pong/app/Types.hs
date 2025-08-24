b0ff467e009a2a53398eba1a5a8297619ade477a
AskBid <noreply>
2025-08-24 23:13:21 +0100

    back to ADTs added instances for Collide

--- 2025-07-12-pong/app/Types.hs
+++ 2025-07-12-pong/app/Types.hs
@@ -1,10 +1,4 @@
-{-# LANGUAGE TypeFamilies, DataKinds   #-}
-{-# LANGUAGE FlexibleInstances         #-}
-{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE MultiParamTypeClasses     #-}
--- one of the key “architectural” advantages of data families over ADTs is that 
--- you don’t have to pattern match on constructors as much, because each type is 
--- a separate type rather than a variant of a single ADT.
 
 module Types where
 
@@ -13,39 +7,18 @@ import qualified Graphics.Rendering.OpenGL as GL
 
 import Helpers
 
--- Haskell needs a way to know which constructor an instance is at runtime.
--- If you have a value of type @Actor@, it could be either a @Ball@ or a @Paddle@.
--- Haskell internally stores a small integer or “tag” that says which constructor it is.
--- Actor memory layout:
--- +------+--------------------+
--- | tag  | fields             |
--- +------+--------------------+
--- | 0    | Ball fields        |
--- | 1    | Paddle fields      |
--- +------+--------------------+
--- the runtime checks the tag to know which branch to take.
--- This adds a tiny runtime overhead and means the memory layout must accommodate 
--- all variants, possibly with padding (alignment different var memory sizes).
---
--- With Data Families is completely separate at compile time.
--- No tag is needed — the compiler already knows the type.
--- You can call move or getPos on without pattern matching, 
--- because the type is known.
+-- Back to ADTs after realisign that:
+-- existentials give heterogeneity but force runtime checks; 
+-- separate typed collections preserve compile-time type safety and performance.
+-- check commit 9bb0dc1 for more notes. (or branch data-families-existential-types)
 
-data ActorTag = BallTag | PaddleTag
-
-data family Actor (t :: ActorTag)
-
--- DataKinds promotes constructors (BallTag and PaddleTag) to be used 
--- on the left side of `=` as Type. From Value Level to Type Level. 
--- the '+BallTag is just "clarity sugar". BallTag would work too.
-data instance Actor 'BallTag = Ball
+data Ball = Ball
   { _ball_pos :: V2 Float
   , _ball_size :: Float
   , _ball_direction :: V2 Float
   }
   
-data instance Actor 'PaddleTag = Paddle
+data Paddle = Paddle
   { _paddle_pos :: V2 Float
   , _paddle_width :: Float
   , _paddle_height :: Float
@@ -55,55 +28,51 @@ data instance Actor 'PaddleTag = Paddle
 class ActorClass a where
   applyCollision :: a -> a
   moveActor :: a -> GameScene -> a
-  hasCollided :: a -> [AnyActor] -> Bool -- [a] should be generalised
   createActorVertices :: a -> IO (GL.VertexArrayObject, GL.BufferObject, Int)
   isInBoundary :: a -> Boundary -> Bool
-  pos :: a -> V2 Float
-  size :: a -> Float
 
--- | Hasell does not usually allow instances for concrete
--- type family application. 
--- that's why to allow for the instances below you need 
--- {-# LANGUAGE FlexibleInstances         #-}
-instance ActorClass (Actor 'BallTag) where
+instance ActorClass Ball where
   applyCollision = undefined
   moveActor = undefined
-  hasCollided _ [] = False
-  hasCollided ball ((Ball pos2 size2 _):bs) =
-    let
-      distanceX = (getX pos) - (getX pos2)
-      distanceY = (getY pos) - (getY pos2)
-      distance = sqrt ((distanceX ** 2) + (distanceY ** 2))
-    in
-      if distance > (size/2) + (size2/2)
-      then hasCollided (Ball pos size mv) bs
-      else True
   createActorVertices = undefined
   isInBoundary = undefined
 
-instance ActorClass (Actor 'PaddleTag) where
+instance ActorClass Paddle where
   applyCollision = undefined
   moveActor = undefined
-  hasCollided _ _ = False 
   createActorVertices = undefined
   isInBoundary = undefined
 
 -- | multiparam typeclasses pragma makes this possible - 
 class Collides a b where
   collides :: a -> b -> Bool
--- TODO even using this Collide with [AnyActor] it means that with the instances of:
--- instance Collides (Actor 'BallTag) (Actor 'BallTag) where ...
--- instance Collides (Actor 'BallTag) (Actor 'PaddleTag) where ...
--- you need to know which type is what in the array [AnyActor] but it is hidden at compile time 
--- Collides is like compile-time overloading: the compiler needs to know the types statically.
--- [AnyActor] is like a box where you’ve hidden the label. You can still open the box at runtime, but the compiler can’t look inside.
--- so the issue is that using Collide a b instances, we need to know what type is the second object, but if we use an ethereogeneous list, we don't know at compile time what type it is, because we hide it behind existential type.. which if we need to use runtime pattern match.. then makes it more wirthwhile to get rid of the ethereogeneous list so that at list we keep the types simple
 
--- | Existential wrapper to store heterogeneous actors
--- This says: “A @AnyActor@ can wrap up any type @a@ that
--- has an ActorClass instance.”
--- requires PRAGMA # ExistentialQuantification #
-data AnyActor = forall a. ActorClass a => AnyActor a
+instance Collides Ball Ball where
+  collides (Ball pos size _) (Ball pos2 size2 _) =
+    let
+      distanceX = getX pos - getX pos2
+      distanceY = getY pos - getY pos2
+      distance = sqrt ((distanceX ** 2) + (distanceY ** 2))
+    in
+      (distance <= ((size / 2) + (size2 / 2))) || False 
+
+-- | TODO need to revisit this calculation, I don't think is correct.
+instance Collides Ball Paddle where
+  collides (Ball posB r _) (Paddle posP w h) =
+    let (xBall, yBall)     = (getX posB, getY posB)
+        (xPaddle, yPaddle) = (getX posP, getY posP)
+    in xBall+r >= xPaddle   && 
+       xBall-r <= xPaddle+w && 
+       yBall+r >= yPaddle   && 
+       yBall-r <= yPaddle+h
+
+instance Collides Paddle Ball where
+  collides = flip collides
+
+data Actors = Actors
+  { balls   :: [Ball]
+  , paddles :: [Paddle]
+  }
 
 --------------------------------------------
 -- Transfered from Main: original Kept Types
@@ -116,6 +85,6 @@ data Boundary = Boundary
   }
 
 data GameScene = GameScene
-  { _gameScene_actors :: [AnyActor]
+  { _gameScene_actors :: Actors
   , _gameScene_boundary :: Boundary
   }
