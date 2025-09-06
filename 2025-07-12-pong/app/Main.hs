6765c4d36fa7958cc01afe7f2176bdf11a3e5b8c
Kyle Jensen <kyle.jensen72@gmail.com>
2025-09-06 12:22:52 -0400

    velocity: wip

--- 2025-07-12-pong/app/Main.hs
+++ 2025-07-12-pong/app/Main.hs
@@ -30,17 +30,17 @@ getUnixTimeMillis = do
   time <- getPOSIXTime
   return $ floor (time * 1000)
 
-data Actor
+data Ball
   = Ball
     { _ball_pos :: V2 Float
     , _ball_size :: Float
-    , _ball_direction :: V2 Float
+    , _ball_velocity :: V2 Float
     }
-  | Paddle
-    { _paddle_pos :: V2 Float
-    , _paddle_width :: Float
-    , _paddle_height :: Float
-    }
+  -- | Paddle
+  --   { _paddle_pos :: V2 Float
+  --   , _paddle_width :: Float
+  --   , _paddle_height :: Float
+  --   }
 
 data Boundary = Boundary
   { _boundary_left :: Float
@@ -49,23 +49,23 @@ data Boundary = Boundary
   , _boundary_bottom :: Float
   }
 
-isInBoundary :: Actor -> Boundary -> Bool
+isInBoundary :: Ball -> Boundary -> Bool
 isInBoundary (Ball pos size _) boundary
   = ((getX pos) - size < _boundary_left boundary)
   || ((getX pos) + size > _boundary_right boundary)
   || ((getY pos) - size < _boundary_top boundary)
   || ((getY pos) + size > _boundary_bottom boundary)
-isInBoundary (Paddle pos width height) boundary
-  = ((getX pos) - width < _boundary_left boundary)
-  || ((getX pos) + width > _boundary_right boundary)
-  || ((getY pos) - height < _boundary_top boundary)
-  || ((getY pos) + height > _boundary_bottom boundary)
+-- isInBoundary (Paddle pos width height) boundary
+--   = ((getX pos) - width < _boundary_left boundary)
+--   || ((getX pos) + width > _boundary_right boundary)
+--   || ((getY pos) - height < _boundary_top boundary)
+--   || ((getY pos) + height > _boundary_bottom boundary)
 
 newtype Mass = Mass Float
 newtype Gravity = Gravity Float
 
 data GameScene = GameScene
-  { _gameScene_actors :: [Actor]
+  { _gameScene_actors :: [Ball]
   , _gameScene_boundary :: Boundary
   }
 
@@ -115,8 +115,15 @@ main = do
 
   (triangleVao, triangleVbo) <- createTriangleVertices
 
-  let playerBall = Ball (V2 0 0) 0.1 (V2 0.0 (-0.0005))
-      otherBall = Ball (V2 0 (-0.5)) 0.1 (V2 0.0 0.0)
+  let playerVelocity = V2 0.0 (-0.5)
+      otherBallVelocity = V2 0.0 0.0
+      playerSize = 0.1
+      otherBallSize = 0.1
+      playerStartPos = V2 0 0
+      otherBallStartPos = V2 0 (-0.5)
+
+      playerBall = Ball playerStartPos playerSize playerVelocity
+      otherBall = Ball otherBallStartPos otherBallSize otherBallVelocity
       boundary = Boundary 0.0 0.0 0.0 0.0
       gameScene = GameScene [playerBall, otherBall] boundary
 
@@ -138,8 +145,38 @@ isQuitEvent e = case e of
     keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeEscape
   _ -> False
 
-createActorVertices :: Actor -> IO (GL.VertexArrayObject, GL.BufferObject, Int)
-createActorVertices (Ball ballPos size _) = do
+
+absV2 :: V2 Float -> V2 Float
+absV2 (V2 x y) = V2 (abs x) (abs y)
+
+perpendicularV2 :: V2 Float -> V2 Float
+perpendicularV2 (V2 x y) = V2 y (-x)
+
+normalizeV2 :: V2 Float -> V2 Float
+normalizeV2 v =
+  let magnitude = sqrt (((getX v) ** 2) + (getY v) ** 2)
+  in V2 (getX v / magnitude) (getY v / magnitude)
+
+scalarMultiply :: V2 Float -> Float -> V2 Float
+scalarMultiply (V2 vx vy) s = V2 (vx * s) (vy * s)
+
+
+-- TODO: Create these functions:
+-- calculateBounce -- multiply velocity by dot product, assume 1:1 mass relationship
+calculateBounceVelocity :: Ball -> Ball -> V2 Float
+calculateBounceVelocity (Ball ballPos1 _ ballVel1) (Ball ballPos2 _ ballVel2) =
+  let collisionVector = ballPos2 - ballPos1
+      perpendicularVector = perpendicularV2 collisionVector
+      normalVector = normalizeV2 perpendicularVector
+      dotProduct = (getX ballVel1) * (getX normalVector) + (getY ballVel1) * (getY normalVector)
+  in
+    ballVel1 - (scalarMultiply normalVector (2 * dotProduct))
+
+
+--calculateBounce actor1 actor2 =
+
+createBallVertices :: Ball -> IO (GL.VertexArrayObject, GL.BufferObject, Int)
+createBallVertices (Ball ballPos size _) = do
   let V2 ballX ballY = ballPos
       ballRadius = size / 2
       vertices
@@ -169,38 +206,7 @@ createActorVertices (Ball ballPos size _) = do
     unwrapV2 :: [V2 Float] -> [Float]
     unwrapV2 [] = []
     unwrapV2 ((V2 x y):xs) = x : y : 0.0 : unwrapV2 xs
-createActorVertices (Paddle pos width height) = do
-  let V2 rectX rectY = pos
-      V2 rectW rectH = V2 width height
 
-      vertices = V.fromList $
-        concatMap (\(V2 x y) -> [x, y, 0.0]) $
-        [ V2 rectX         rectY
-        , V2 (rectX+rectW) rectY
-        , V2 rectX         (rectY+rectH)
-        , V2 rectX         (rectY+rectH)
-        , V2 (rectX+rectW) rectY
-        , V2 (rectX+rectW) (rectY+rectH)
-        ]
-
-      numVertices = V.length vertices `div` 3
-
-  vao <- GL.genObjectName
-  GL.bindVertexArrayObject $= Just vao
-
-  vbo <- GL.genObjectName
-  GL.bindBuffer GL.ArrayBuffer $= Just vbo
-  V.unsafeWith vertices $ \ptr ->
-    GL.bufferData GL.ArrayBuffer $= (fromIntegral (V.length vertices * 4), ptr, GL.StaticDraw)
-
-  GL.vertexAttribArray (GL.AttribLocation 0) $= GL.Enabled
-  GL.vertexAttribPointer (GL.AttribLocation 0) $=
-    (GL.ToFloat, GL.VertexArrayDescriptor 3 GL.Float 0 nullPtr)
-
-  GL.bindBuffer GL.ArrayBuffer $= Nothing
-  GL.bindVertexArrayObject $= Nothing
-
-  return (vao, vbo, numVertices)
 
 createCircleVertices :: V2 Float -> Float -> [V2 Float]
 createCircleVertices pos radius = (calculateCircumferencePoint pos radius) <$> [1..360]
@@ -254,7 +260,7 @@ compileShaderFromSource shaderType source = do
     putStrLn $ "Shader compile log: " ++ log
   return shader
 
-hasCollided :: Actor -> [Actor] -> Bool
+hasCollided :: Ball -> [Ball] -> Bool
 hasCollided _ [] = False
 hasCollided (Ball pos size mv) ((Ball pos2 size2 _):bs) =
   let
@@ -267,13 +273,15 @@ hasCollided (Ball pos size mv) ((Ball pos2 size2 _):bs) =
     else True
 hasCollided _ _ = False
 
-applyCollision :: Actor -> Actor
-applyCollision (Ball pos size mv) = Ball pos size (-mv)
-applyCollision (Paddle pos width height) = Paddle pos width height
+applyCollision :: Ball -> Ball -> Ball
+applyCollision ball1@(Ball pos size mv) ball2@(Ball pos2 size2 mv2) =
+  let newVelocity = calculateBounceVelocity ball1 ball2
+  in Ball pos size newVelocity
 
-moveActor :: Actor -> GameScene -> Actor
-moveActor (Ball pos size mv) scene = Ball (pos + mv) size mv
-moveActor (Paddle pos width height) scene = Paddle pos width height
+moveBall :: Ball -> GameScene -> Float -> Ball
+moveBall (Ball pos size (V2 velX velY)) scene timeDelta =
+  let movementVector = V2 (velX * timeDelta) (velY * timeDelta)
+  in Ball (pos + movementVector) size (V2 velX velY)
 
 gameLoop :: Window -> GL.VertexArrayObject -> GameScene -> Integer -> GL.Program -> IO ()
 gameLoop window triangleVao gameScene prevTime program = do
@@ -281,23 +289,27 @@ gameLoop window triangleVao gameScene prevTime program = do
   currentUnixTime <- getUnixTimeMillis
 
   let quit = any (isQuitEvent . eventPayload) events
-      timeDelta = currentUnixTime - prevTime
+      timeDelta = (fromInteger (currentUnixTime - prevTime)) / 1000
+
+  --putStrLn $ "Time Delta " ++ show timeDelta
 
   -- Clear the screen
   GL.clearColor $= GL.Color4 0 0 0 1
   GL.clear [GL.ColorBuffer]
 
   let playerBall = (_gameScene_actors gameScene) !! 0
-      otherBall = moveActor ((_gameScene_actors gameScene) !! 1) gameScene
+      otherBall = moveBall ((_gameScene_actors gameScene) !! 1) gameScene timeDelta
       collisionOccurred = hasCollided playerBall [otherBall]
-      collidedBall = if collisionOccurred then applyCollision playerBall else playerBall
-      newBall = moveActor collidedBall gameScene
+      collidedBall = if collisionOccurred then applyCollision playerBall otherBall else playerBall
+      newBall = moveBall collidedBall gameScene timeDelta
 
   -- Log collision status
   putStrLn $ "Frame at " ++ show currentUnixTime ++ "ms: Collision " ++ if collisionOccurred then "detected" else "not detected"
+  putStrLn $ "calculateBounceVelocity ball1 ball2" ++ show (calculateBounceVelocity playerBall otherBall)
 
-  (ballVao, ballVbo, ballNumVertices) <- createActorVertices newBall
-  (ball2Vao, ball2Vbo, ball2NumVertices) <- createActorVertices otherBall
+
+  (ballVao, ballVbo, ballNumVertices) <- createBallVertices newBall
+  (ball2Vao, ball2Vbo, ball2NumVertices) <- createBallVertices otherBall
 
   GL.currentProgram $= Just program
   GL.bindVertexArrayObject $= Just ballVao
