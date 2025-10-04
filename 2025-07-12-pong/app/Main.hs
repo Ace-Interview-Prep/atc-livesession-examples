09324159b0c275509e6c280f104dae18fcb24401
Kyle Jensen <kyle.jensen72@gmail.com>
2025-10-04 12:04:12 -0400

    refactor - currently broken due to issue with translation matrix - also caching issues

--- 2025-07-12-pong/app/Main.hs
+++ 2025-07-12-pong/app/Main.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BlockArguments #-}
 
 module Main where
 
-import SDL
+import SDL hiding (createWindow, origin)
 import SDL.Video.OpenGL (glCreateContext)
 import Prelude hiding (log)
 import Foreign.Ptr
@@ -16,60 +17,17 @@ import qualified Graphics.Rendering.OpenGL as GL
 import qualified Data.Vector.Storable as V
 import qualified Debug.Trace as DT
 import Graphics.Rendering.OpenGL.GL.CoordTrans
-import Shaders (vertexShaderSource, fragmentShaderSource)
-
+import Shaders (vertexShaderSource, fragmentShaderSource, createShaderProgram)
+import Window (createWindow)
 import Data.Time.Clock.POSIX (getPOSIXTime)
+import qualified Linear.V2 as L
+import Data.IORef
 
-getX :: V2 a -> a
-getX (V2 x _) = x
+import Debug.Trace as DT
 
-getY :: V2 a -> a
-getY (V2 _ y) = y
-
-getUnixTimeMillis :: IO Integer
-getUnixTimeMillis = do
-  time <- getPOSIXTime
-  return $ floor (time * 1000)
-
-data Ball
-  = Ball
-    { _ball_pos :: V2 Float
-    , _ball_size :: Float
-    , _ball_velocity :: V2 Float
-    , _label :: String
-    } deriving Show
-  -- | Paddle
-  --   { _paddle_pos :: V2 Float
-  --   , _paddle_width :: Float
-  --   , _paddle_height :: Float
-  --   }
-
-data Boundary = Boundary
-  { _boundary_left :: Float
-  , _boundary_right :: Float
-  , _boundary_top :: Float
-  , _boundary_bottom :: Float
-  }
-
-isInBoundary :: Ball -> Boundary -> Bool
-isInBoundary (Ball pos size _ _) boundary
-  = ((getX pos) - size < _boundary_left boundary)
-  || ((getX pos) + size > _boundary_right boundary)
-  || ((getY pos) - size < _boundary_top boundary)
-  || ((getY pos) + size > _boundary_bottom boundary)
--- isInBoundary (Paddle pos width height) boundary
---   = ((getX pos) - width < _boundary_left boundary)
---   || ((getX pos) + width > _boundary_right boundary)
---   || ((getY pos) - height < _boundary_top boundary)
---   || ((getY pos) + height > _boundary_bottom boundary)
-
-newtype Mass = Mass Float
-newtype Gravity = Gravity Float
-
-data GameScene = GameScene
-  { _gameScene_actors :: [Ball]
-  , _gameScene_boundary :: Boundary
-  }
+import Ball
+import Rendering
+import qualified Rendering as R
 
 gravity :: Gravity
 gravity = Gravity 9.80665
@@ -80,45 +38,25 @@ screenWidth = 800
 screenHeight :: (Num a) => a
 screenHeight = 800
 
+windowConfig :: WindowConfig
+windowConfig = defaultWindow
+               { windowInitialSize = V2 screenWidth screenHeight
+               , windowGraphicsContext = OpenGLContext defaultOpenGL
+                                         { glProfile = Core Normal 3 3
+                                         }
+               }
+
 main :: IO ()
 main = do
-  initializeAll
+  SDL.initializeAll
+  window <- createWindow windowConfig "Ping Pong"
+  glContext <- createGLContext window
+  setViewport (Position 0 0) (Size screenWidth screenHeight)
+  program <- createShaderProgram
+  setShaderProgram program
 
-  let windowConfig = defaultWindow
-        { windowInitialSize = V2 screenWidth screenHeight
-        , windowGraphicsContext = OpenGLContext defaultOpenGL
-                                  { glProfile = Core Normal 3 3
-                                  }
-        }
-
-  putStrLn $ show (windowGraphicsContext windowConfig)
-
-  windowResult <- try $ createWindow "Ping Pong" windowConfig
-
-  window <- case windowResult of
-    Left (err :: SomeException) -> throwIO $ userError $ "Window creation failed: " ++ show err
-    Right w -> do
-      putStrLn "Window created successfully"
-      return w
-
-  glContextResult <- try $ glCreateContext window
-  glContext <- case glContextResult of
-    Left (err :: SomeException) -> throwIO $ userError $ "OpenGL context creation failed: " ++ show err
-    Right ctx -> return ctx
-
-  GL.viewport $= (Position 0 0, Size screenWidth screenHeight)
-
-  programResult <- try $ createShaderProgramFromSource vertexShaderSource fragmentShaderSource
-  program <- case programResult of
-    Left (err :: SomeException) -> throwIO $ userError $ "Shader program creation failed: " ++ show err
-    Right prog -> return prog
-
-  GL.currentProgram $= Just program
-
-  (triangleVao, triangleVbo) <- createTriangleVertices
-
-  let playerVelocity = V2 0.0 (-0.05)
-      otherBallVelocity = V2 0.0 (0.05)
+  let playerVelocity = V2 0.0 (0.1)
+      otherBallVelocity = V2 0.0 (-0.1)
       playerSize = 0.1
       otherBallSize = 0.1
       playerStartPos = V2 0 0
@@ -127,16 +65,27 @@ main = do
       playerBall = Ball playerStartPos playerSize playerVelocity "Player"
       otherBall = Ball otherBallStartPos otherBallSize otherBallVelocity "Other"
       boundary = Boundary 0.0 0.0 0.0 0.0
-      gameScene = GameScene [playerBall, otherBall] boundary
+      gameState = GameState [playerBall, otherBall] boundary
+      scene = Scene []
+
+  -- Create initial renderables
+  playerBallR <- createRenderable playerBall
+  otherBallR <- createRenderable otherBall
+
+  let scene = Scene [ playerBallR
+                    , otherBallR
+                    ]
+
+  modelLoc <- GL.get $ GL.uniformLocation program "uModel"
+  let render = renderScene window program modelLoc
 
   currentUnixTime <- getUnixTimeMillis
-  gameLoop window triangleVao gameScene currentUnixTime program
 
-  GL.deleteObjectNames [program]
-  GL.deleteObjectNames [triangleVao]
-  GL.deleteObjectNames [triangleVbo]
-  glDeleteContext glContext
-  destroyWindow window
+  finalScene <- gameLoop render scene gameState currentUnixTime
+  let vaos = vao <$> (_renderables finalScene)
+      vbos = vbo <$> (_renderables finalScene)
+
+  R.destroyAll window glContext [program] vaos vbos
   quit
 
 isQuitEvent :: EventPayload -> Bool
@@ -169,95 +118,10 @@ calculateBounceVelocity (Ball ballPos1 _ ballVel1 _) (Ball ballPos2 _ ballVel2 _
     normalVector      = normalizeV2 collisionVector
     relativeVelocity  = ballVel1 - ballVel2
     dotProduct        = (getX relativeVelocity) * (getX normalVector) + (getY relativeVelocity) * (getY normalVector)
-    velocityChange    = scalarMultiply normalVector (2 * dotProduct)
+    velocityChange    = scalarMultiply normalVector dotProduct
   in
     ballVel1 - velocityChange
 
-
-createBallVertices :: Ball -> IO (GL.VertexArrayObject, GL.BufferObject, Int)
-createBallVertices (Ball ballPos size _ _) = do
-  let V2 ballX ballY = ballPos
-      ballRadius = size / 2
-      vertices
-        = V.fromList
-        $ concat [ [ballX, ballY, 0.0]
-                 ] ++ unwrapV2 (createCircleVertices ballPos ballRadius)
-      numVertices = (V.length vertices) `div` 3
-
-  vao <- GL.genObjectName
-  GL.bindVertexArrayObject $= Just vao
-
-  vbo <- GL.genObjectName
-  GL.bindBuffer GL.ArrayBuffer $= Just vbo
-  V.unsafeWith vertices $ \ptr ->
-    GL.bufferData GL.ArrayBuffer $= (fromIntegral (V.length vertices * 4), ptr, GL.StaticDraw)
-
-  GL.vertexAttribArray (GL.AttribLocation 0) $= GL.Enabled
-  GL.vertexAttribPointer (GL.AttribLocation 0) $=
-    (GL.ToFloat, GL.VertexArrayDescriptor 3 GL.Float 0 nullPtr)
-
-  GL.bindBuffer GL.ArrayBuffer $= Nothing
-  GL.bindVertexArrayObject $= Nothing
-
-  return (vao, vbo, numVertices)
-
-  where
-    unwrapV2 :: [V2 Float] -> [Float]
-    unwrapV2 [] = []
-    unwrapV2 ((V2 x y):xs) = x : y : 0.0 : unwrapV2 xs
-
-
-createCircleVertices :: V2 Float -> Float -> [V2 Float]
-createCircleVertices pos radius = (calculateCircumferencePoint pos radius) <$> [1..360]
-
-calculateCircumferencePoint :: V2 Float -> Float -> Int -> V2 Float
-calculateCircumferencePoint pos r th = V2 (r * (sin (fromIntegral th))) (r * (cos (fromIntegral th))) + pos
-
-createTriangleVertices :: IO (GL.VertexArrayObject, GL.BufferObject)
-createTriangleVertices = do
-  let vertices = V.fromList
-        [ 0.0, 0.5, 0.0
-        , (-0.5), (-0.5), 0.0
-        , 0.5, -0.5, 0.0
-        ] :: V.Vector Float
-
-  vao <- GL.genObjectName
-  GL.bindVertexArrayObject $= Just vao
-
-  vbo <- GL.genObjectName
-  GL.bindBuffer GL.ArrayBuffer $= Just vbo
-  V.unsafeWith vertices $ \ptr ->
-    GL.bufferData GL.ArrayBuffer $= (fromIntegral (V.length vertices * 4), ptr, GL.StaticDraw)
-
-  GL.vertexAttribArray (GL.AttribLocation 0) $= GL.Enabled
-  GL.vertexAttribPointer (GL.AttribLocation 0) $=
-    (GL.ToFloat, GL.VertexArrayDescriptor 3 GL.Float 0 nullPtr)
-
-  GL.bindBuffer GL.ArrayBuffer $= Nothing
-  GL.bindVertexArrayObject $= Nothing
-
-  return (vao, vbo)
-
-createShaderProgramFromSource :: ByteString -> ByteString -> IO GL.Program
-createShaderProgramFromSource vsrc fsrc = do
-  vertexShader <- compileShaderFromSource GL.VertexShader vsrc
-  fragmentShader <- compileShaderFromSource GL.FragmentShader fsrc
-  program <- GL.createProgram
-  GL.attachShader program vertexShader
-  GL.attachShader program fragmentShader
-  GL.linkProgram program
-  return program
-
-compileShaderFromSource :: GL.ShaderType -> ByteString -> IO GL.Shader
-compileShaderFromSource shaderType source = do
-  shader <- GL.createShader shaderType
-  GL.shaderSourceBS shader $= source
-  _ <- GL.compileShader shader
-  ok <- GL.get (GL.compileStatus shader)
-  unless ok $ do
-    log <- GL.get (GL.shaderInfoLog shader)
-    putStrLn $ "Shader compile log: " ++ log
-  return shader
 
 hasCollided :: Ball -> [Ball] -> Bool
 hasCollided _ [] = False
@@ -272,55 +136,90 @@ hasCollided (Ball pos size mv label) ((Ball pos2 size2 _ _):bs) =
     else True
 hasCollided _ _ = False
 
+
 applyCollision :: Ball -> Ball -> Ball
 applyCollision ball1@(Ball pos size mv label1) ball2@(Ball pos2 size2 mv2 label2) =
   let newVelocity = calculateBounceVelocity ball1 ball2
-  in Ball pos size newVelocity' label1
+  in Ball pos size newVelocity label1
 
-moveBall :: Ball -> GameScene -> Float -> Ball
-moveBall (Ball pos size (V2 velX velY) label) scene timeDelta =
+moveBall :: Ball -> GameState -> Float -> Ball
+moveBall (Ball pos size (V2 velX velY) label) state timeDelta =
   let movementVector = V2 (velX * timeDelta) (velY * timeDelta)
   in Ball (pos + movementVector) size (V2 velX velY) label
 
-gameLoop :: Window -> GL.VertexArrayObject -> GameScene -> Integer -> GL.Program -> IO ()
-gameLoop window triangleVao gameScene prevTime program = do
+
+runGameStep :: GameState -> Float -> GameState
+runGameStep gameState timeDelta =
+  let playerBall = (_gameState_actors gameState) !! 0
+      otherBall = (_gameState_actors gameState) !! 1
+      collisionOccurred = hasCollided playerBall [otherBall]
+      collidedBall = if collisionOccurred then applyCollision playerBall otherBall else playerBall
+      collidedBall' = if collisionOccurred then applyCollision otherBall playerBall else otherBall
+      newBall = moveBall collidedBall gameState timeDelta
+      newBall2 = moveBall collidedBall' gameState timeDelta
+  in
+    GameState [newBall, newBall2] (_gameState_boundary gameState)
+
+runRenderScene :: R.Scene -> GameState -> R.Scene
+runRenderScene scene gameState =
+  let newBall = (_gameState_actors gameState) !! 0
+      newBall2 = (_gameState_actors gameState) !! 1
+      updatedPlayerBallR = ((_renderables scene) !! 0) { origin = _ball_pos newBall }
+      updatedOtherBallR = ((_renderables scene) !! 1) { origin = _ball_pos newBall2 }
+  in
+    R.Scene [updatedPlayerBallR, updatedOtherBallR]
+
+
+gameLoop :: (R.Scene -> IO ()) -> R.Scene -> GameState -> Integer -> IO (R.Scene)
+gameLoop render scene gameState prevTime = do
   events <- pollEvents
   currentUnixTime <- getUnixTimeMillis
 
   let quit = any (isQuitEvent . eventPayload) events
       timeDelta = (fromInteger (currentUnixTime - prevTime)) / 1000
 
-  --putStrLn $ "Time Delta " ++ show timeDelta
+  -- Run game step
+  case quit of
+    False -> do
+      let updatedGameState = runGameStep gameState timeDelta
+          updatedScene = runRenderScene scene updatedGameState
 
-  -- Clear the screen
-  GL.clearColor $= GL.Color4 0 0 0 1
-  GL.clear [GL.ColorBuffer]
+      render updatedScene
+      gameLoop render updatedScene updatedGameState currentUnixTime
 
-  let playerBall = (_gameScene_actors gameScene) !! 0
-      otherBall = moveBall ((_gameScene_actors gameScene) !! 1) gameScene timeDelta
-      collisionOccurred = hasCollided playerBall [otherBall]
-      collidedBall = if collisionOccurred then applyCollision playerBall otherBall else playerBall
-      collidedBall' = if collisionOccurred then applyCollision otherBall playerBall else otherBall
-      newBall = moveBall collidedBall gameScene timeDelta
-      newBall2 = moveBall collidedBall' gameScene timeDelta
-
-  -- Log collision status
-  --putStrLn $ "Frame at " ++ show currentUnixTime ++ "ms: Collision " ++ if collisionOccurred then "detected" else "not detected"
-  --putStrLn $ "calculateBounceVelocity ball1 ball2" ++ show (calculateBounceVelocity playerBall otherBall)
+    True ->
+      return scene
 
 
-  (ballVao, ballVbo, ballNumVertices) <- createBallVertices newBall
-  (ball2Vao, ball2Vbo, ball2NumVertices) <- createBallVertices newBall2
+getX :: V2 a -> a
+getX (V2 x _) = x
 
-  GL.currentProgram $= Just program
-  GL.bindVertexArrayObject $= Just ballVao
-  GL.drawArrays GL.TriangleFan 0 (fromIntegral ballNumVertices)
+getY :: V2 a -> a
+getY (V2 _ y) = y
 
-  GL.bindVertexArrayObject $= Just ball2Vao
-  GL.drawArrays GL.TriangleFan 0 (fromIntegral ball2NumVertices)
+getUnixTimeMillis :: IO Integer
+getUnixTimeMillis = do
+  time <- getPOSIXTime
+  return $ floor (time * 1000)
 
-  -- Swap buffers
-  glSwapWindow window
+data Boundary = Boundary
+  { _boundary_left :: Float
+  , _boundary_right :: Float
+  , _boundary_top :: Float
+  , _boundary_bottom :: Float
+  }
 
-  let updatedGameScene = GameScene [newBall, newBall2] (_gameScene_boundary gameScene)
-  unless quit $ gameLoop window triangleVao updatedGameScene currentUnixTime program
+isInBoundary :: Ball -> Boundary -> Bool
+isInBoundary (Ball pos size _ _) boundary
+  = ((getX pos) - size < _boundary_left boundary)
+  || ((getX pos) + size > _boundary_right boundary)
+  || ((getY pos) - size < _boundary_top boundary)
+  || ((getY pos) + size > _boundary_bottom boundary)
+
+newtype Mass = Mass Float
+newtype Gravity = Gravity Float
+
+data GameState = GameState
+  { _gameState_actors :: [Ball]
+  , _gameState_boundary :: Boundary
+  }
--- 2025-07-12-pong/app/Shaders.hs
+++ 2025-07-12-pong/app/Shaders.hs
@@ -1,22 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Shaders where
 
+import SDL
 import Prelude hiding (unlines)
+import Control.Monad (unless)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import qualified Graphics.Rendering.OpenGL as GL
+import Control.Exception (SomeException, try, throwIO)
+
 
 vertexShaderSource :: ByteString
 vertexShaderSource = BS.unlines
   [ "#version 330 core"
   , "layout (location = 0) in vec3 aPos;"
   , "out vec3 vPos;"
+  , "uniform mat4 uModel;"
   , "void main() {"
   , "    gl_Position = vec4(aPos, 1.0);"
   , "    vPos = aPos;"
   , "}"
   ]
-
 
 fragmentShaderSource :: ByteString
 fragmentShaderSource = BS.unlines
@@ -25,7 +31,35 @@ fragmentShaderSource = BS.unlines
   , "out vec4 FragColor;"
   , "void main() {"
   , "    vec3 normalColor = normalize(vPos);"
---  , "    FragColor = vec4(abs(normalColor), 1.0);"
-  , "    FragColor = vec4(1.0, 1.0, 1.0, 1.0);"
+  , "    FragColor = vec4(1.0, 1.0, 0.0, 1.0);"
   , "}"
   ]
+
+createShaderProgram :: IO GL.Program
+createShaderProgram = do
+  programResult <- try $ createShaderProgramFromSource vertexShaderSource fragmentShaderSource
+  program <- case programResult of
+    Left (err :: SomeException) -> throwIO $ userError $ "Shader program creation failed: " ++ show err
+    Right prog -> return prog
+  return program
+
+createShaderProgramFromSource :: ByteString -> ByteString -> IO GL.Program
+createShaderProgramFromSource vsrc fsrc = do
+  vertexShader <- compileShaderFromSource GL.VertexShader vsrc
+  fragmentShader <- compileShaderFromSource GL.FragmentShader fsrc
+  program <- GL.createProgram
+  GL.attachShader program vertexShader
+  GL.attachShader program fragmentShader
+  GL.linkProgram program
+  return program
+
+compileShaderFromSource :: GL.ShaderType -> ByteString -> IO GL.Shader
+compileShaderFromSource shaderType source = do
+  shader <- GL.createShader shaderType
+  GL.shaderSourceBS shader $= source
+  _ <- GL.compileShader shader
+  ok <- GL.get (GL.compileStatus shader)
+  unless ok $ do
+    log' <- GL.get (GL.shaderInfoLog shader)
+    putStrLn $ "Shader compile log: " ++ log'
+  return shader
